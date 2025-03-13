fbf832d94cef442ecae0e5bcd0af3ddce80a8224
shaanis <mhdshanis3@gmail.com>
2025-03-13 14:31:04 +0530

    changes

--- src/services/serverurl.js
+++ src/services/serverurl.js
@@ -1,4 +1,5 @@
-const serverurl = 'https://localguide-server-1.onrender.com'
+// const serverurl = 'https://localguide-server-1.onrender.com'
+const serverurl = 'http://localhost:3000'
 
 
 export default serverurl
